2200b1219988843754560eb50351861599def587
paty <patygmail.com>
2022-10-12 08:36:20 -0500

    oijmei

--- cosumir_api/app.js
+++ cosumir_api/app.js
@@ -1,13 +1,13 @@
 const consumirAPI=async ()=>{
-    const respuestas=await fetch('https://jsonplaceholder.typicode.com/todos/1')
-    const json=await respuestas.json();
+    const respuesta= await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    const json=await respuesta.json();
     console.log("user id:"+json.userID);
-    console.log( "id:"+json.userID);
-    console.log("titulo:" +json.userID);
-    console.log("completado:"+json.userID);
+    console.log( "id:"+json.ID);
+    console.log("titulo:" +json.title);
+    console.log("completado:"+json.completed);
     let div=document.createElement("div");
     div.className="cajita";
-    div.innerHTML`<p>user id:${json.userID}</p>
+    div.innerHTML`<p><span>user id:${json.userID}</p>
     <p> id:${json.id}</p>
     <p>titulo:${json.titlr}</p>
     <p>completado:${jsom.completed}</p>`;
